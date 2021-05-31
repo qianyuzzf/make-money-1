98ed253c6ad69a88937ceb0ff9a50885c4458074
qianyu <280624745@qq.com>
2021-05-31 16:40:01 +0800

    创建三个页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,33 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/money'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/money',
+    name: 'money',
+    component: Money
+  },
+  {
+    path: '/labels',
+    name: 'labels',
+    component: Labels
+  },
+  {
+    path: '/statistics',
+    name: 'statistics',
+    component: Statistics
   }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
-})
+});
 
-export default router
+export default router;
