97f122e86aff351bb42a4e4ed4c2ed9010243647
qianyu <280624745@qq.com>
2021-05-31 22:52:14 +0800

    引入 icon

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import router from './router';
 import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
 const app = createApp(App);
 app.component('Nav', Nav);
 app.component('Layout', Layout);
+app.component('Icon', Icon);
 app.use(store).use(router);
 app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      //只包含 icons 目录
+      .include.add(dir).end()
+      //配置 loader
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+    //extract: false 不要解析出文件
+
+    //配置插件
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+
+    //其他 svg loader 排除 icons 目录
+    config.module.rule('svg').exclude.add(dir)
+  }
 }
