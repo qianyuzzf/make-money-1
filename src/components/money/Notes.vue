470feb8c412969bfbe8ef1577e04416600507d53
qianyu <280624745@qq.com>
2021-06-01 20:18:39 +0800

    模块化 Money.vue 组件

---
+++ src/components/money/Notes.vue
@@ -0,0 +1,33 @@
+<template>
+  <label class="notes">
+    <span class="name">备注</span>
+    <input type="text" placeholder="请输入备注信息">
+  </label>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notes'
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  padding-left: 16px;
+  background: #f5f5f5;
+  display: flex;
+  align-items: center;
+
+  > .name {
+    padding-right: 16px;
+  }
+
+  > input {
+    padding: 16px 16px 16px 0;
+    border: none;
+    flex-grow: 1;
+    background: transparent;
+  }
+}
+</style>
---
+++ src/components/money/Types.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="types">
+    <li class="selected">支出</li>
+    <li>收入</li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Types'
+};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  display: flex;
+  background: #c4c4c4;
+  font-size: 24px;
+  text-align: center;
+
+  > li {
+    width: 50%;
+    padding: 14px 0;
+    position: relative;
+
+    &.selected {
+      &::after {
+        content: '';
+        width: 100%;
+        height: 4px;
+        bottom: 0;
+        left: 0;
+        background: #333333;
+        position: absolute;
+      }
+    }
+  }
+}
+</style>
