52490347891efec81a8736c8d097a4e8a7e4b946
qianyu <280624745@qq.com>
2021-06-01 14:56:01 +0800

    解决 SVG 自带 fill 属性问题

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,22 @@ module.exports = {
       //只包含 icons 目录
       .include.add(dir).end()
       //配置 loader
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
-    //extract: false 不要解析出文件
+      .use('svg-sprite-loader').loader('svg-sprite-loader')
+      .options({extract: false}).end()
+      //extract: false 不要解析出文件
+      //使用 svgo-loader 删除 svg 的 fill 属性
+      .before('svg-sprite-loader')
+      .use('svgo-loader').loader('svgo-loader')
+      .options({
+        plugins: [
+          {removeAttrs: {attrs: 'path:fill'}}
+        ]
+      }).end()
+    // .options 可写如下详细代码
+    // .tap(options => ({
+    //   ...options,
+    //   plugins: [{removeAttrs: {attrs: 'path:fill'}}]
+    // })).end()
 
     //配置插件
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
