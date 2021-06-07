f16c0442dbccd1f462b918b8467838aeeaa160ec
qianyu <280624745@qq.com>
2021-06-07 15:47:27 +0800

    添加二维码扫描功能

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,15 @@ window.onload = function () {
     window.scroll(0, 100);
   }, 0);
 };
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('使用手机浏览效果更佳');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 5px rgba(0,0,0,0.8)';
+  document.body.appendChild(img);
+}
