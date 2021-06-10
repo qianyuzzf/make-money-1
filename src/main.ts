973eee46bd5ef422b2d4f20503c3b781150699e4
qianyu <280624745@qq.com>
2021-06-10 09:39:04 +0800

    去除二维码提示

--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,4 @@ window.onload = function () {
 
 if (document.documentElement.clientWidth > 500) {
   window.alert('使用手机浏览效果更佳');
-  const img = document.createElement('img');
-  img.src = './qrcode.png';
-  img.style.position = 'fixed';
-  img.style.left = '50%';
-  img.style.top = '50%';
-  img.style.transform = 'translate(-50%,-50%)';
-  img.style.boxShadow = '0 0 5px rgba(0,0,0,0.8)';
-  document.body.appendChild(img);
 }
